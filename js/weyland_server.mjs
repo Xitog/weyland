3e93d966658ca4bcdbd2e77e54c5dce3a3457119
Xitog <damien.gouteux@gmail.com>
2022-01-18 15:51:07 +0100

    Weyland now only a lexer

--- js/weyland_server.mjs
+++ js/weyland_server.mjs
@@ -1,8 +1,5 @@
 import * as http from "http";
-import * as url from "url";
-import { Regex } from "./weyland.mjs";
-import { Lexer } from "./lexer.mjs";
-import { Language, INTEGER, IDENTIFIER } from "./languages.mjs";
+import { RECOGNIZED_LANGUAGES, LEXERS} from "./heretic.mjs";
 
 const host = 'localhost';
 const port = 7000;
@@ -38,91 +35,71 @@ const requestListener_action = function(request, response)
         }
     });
     request.on('end', function() {
-        // const query = url.parse(request.url, true).query;
-        //respond.setHeader("Content-Type", "text/html");
         response.setHeader("Content-Type", "application/json; charset=utf8");
         response.writeHead(200);
-        //respond.end("<h1>Hello!<h1>");
         let params_array = queryData.split("&");
         let params_dict = {};
         for (const p of params_array)
         {
             let kv = p.split('=');
-            params_dict[kv[0]] = kv[1].replace(/\+/g, ' ').replace(/%2B/g, '+');
-            console.log(p, '=>', params_dict[kv[0]]);
+            if (kv.length > 1)
+            {
+                params_dict[kv[0]] = kv[1].replace(/\+/g, ' ').replace(/%2B/g, '+').replace(/%3D/g, '=');
+                console.log(p, '=>', params_dict[kv[0]]);
+            }
         }
         // Parameters
         let id = null;
-        let method = null;
-        let pattern = null;
         let text = null;
+        let lang = null;
         let output = "JSON";
-        if ("method" in params_dict)
-        {
-            switch(params_dict["method"])
-            {
-                case "regex":
-                    method = "regex";
-                    break;
-                case "lex":
-                    method = "lex";
-                    break;
-            }
-        }
         if ("id" in params_dict)
         {
             id = params_dict["id"];
-        }
-        if ("pattern" in params_dict)
-        {
-            pattern = params_dict["pattern"];
+            console.log(`Id set to ${id}`);
         }
         if ("text" in params_dict)
         {
             text = params_dict["text"];
-            console.log(text);
+            console.log(`Text received: ${text}`);
         }
         if ("output" in params_dict)
         {
             output = params_dict["output"];
+            console.log(`Ouput set to ${output}`);
+        }
+        if ("lang" in params_dict)
+        {
+            lang = params_dict["lang"];
+            console.log(`Lang set to ${lang}`);
         }
         // Checks
         let error = false;
         let error_msg = null;
-        if (method === null)
+        if (lang === null)
         {
             error = true;
-            error_msg = "No method defined.";
-        }
-        else if (method === "regex")
-        {
-            if (id === null)
+            error_msg = "No lang defined.";
+        } else {
+            if (!RECOGNIZED_LANGUAGES.includes(lang))
             {
                 error = true;
-                error_msg = "No id defined for method regex.";
-            }
-            if (pattern === null)
-            {
-                error = true;
-                error_msg = "No pattern defined for method regex.";
+                error_msg = `Lang ${lang} is not handled.`;
             }
         }
-        else if (method === "lex")
+        if (id === null)
         {
-            if (id === null)
-            {
-                error = true;
-                error_msg = "No id defined for method lex.";
-            }
-            if (text === null)
-            {
-                error = true;
-                error_msg = "No text defined for method lex.";
-            }
+            error = true;
+            error_msg = "No id defined for method lex.";
+        }
+        if (text === null)
+        {
+            error = true;
+            error_msg = "No text defined for method lex.";
         }
         if (error)
         {
-            respond.end(JSON.stringify(
+            response.end(JSON.stringify(
                     {
                         "error": error,
                         "message": error_msg
@@ -132,28 +109,18 @@ const requestListener_action = function(request, response)
             return;
         }
         // Actions
-        let result = null;
-        if (method === "regex")
-        {
-            let regex = new Regex(pattern, true);
-            // res = regex.match(txt).toString();
-            result = regex.toString();
-        }
-        else if (method === "lex")
-        {
-            let lang = new Language("Pipo", {'keywords': ['if'], 'int': INTEGER, 'id': IDENTIFIER, 'spaces': ' +', 'operator': ['==', '\\+']});
-            let lexer = new Lexer(lang);
-            let tokens = lexer.lex(text);
-            result = lexer.to_html(null, tokens);
-        }
+        let lexer = LEXERS[lang];
+        let tokens = lexer.lex(text);
+        let result = lexer.to_html(null, tokens);
         response.end(
             JSON.stringify(
                 {
                     "raw": queryData,
                     "params_array": params_array,
                     "params_dict": params_dict,
-                    "method_called": method,
                     "id": id,
+                    "lang": lang,
+                    "tokens": tokens,
                     "result": result
                 }
             )
